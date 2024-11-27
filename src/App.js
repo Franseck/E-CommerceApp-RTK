6301620a3992b9aa84e10d48713b11699287dbb2
Seckin Ozturk <franseck102@gmail.com>
2024-11-26 22:02:25 -0500

    dependency packages setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { ToastContainer } from "react-toastify"
 import AppRouter from "./router/AppRouter"
+
+
 
 const App = () => {
   return (
     <div>
       <AppRouter/>
+  
+  <ToastContainer/>
     </div>
   )
 }
