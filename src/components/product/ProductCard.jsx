f27d486f50ca9c9caa2b00682fbcecd3107cb8e5
Seckin Ozturk <franseck102@gmail.com>
2024-12-01 22:47:59 -0500

    single product page

--- src/components/product/ProductCard.jsx
+++ src/components/product/ProductCard.jsx
@@ -28,18 +28,7 @@ const ProductCard = ({ id, name, text, img, price, colors }) => {
         </CardBody>
         <CardFooter divider className="flex items-center justify-between py-3">
           <Typography variant="small" className="font-general text-blue-gray-900">{price}$</Typography>
-          <Typography variant="small" color="gray" className="flex gap-1">
-            {colors?.map((color, index) => {
-              return (
-                <i
-                  className="fas fa-map-marker-alt fa-sm mt-[3px] rounded-full p-2 mr-4 "
-                  key={index}
-                  style={{ backgroundColor: color }}
-                ></i>
-              );
-            })}
-          </Typography>
-        </CardFooter>
+           </CardFooter>
       </Card>
     </Link>
     </div>
